7e22bdcdcc107aaa8f04be732ddee6f94f4c59d9
Aezurly <66571923+Aezurly@users.noreply.github.com>
2024-02-10 12:40:48 -0500

    blob following + dark text

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,14 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const blob = document.getElementById("blob-mouse");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob?.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
